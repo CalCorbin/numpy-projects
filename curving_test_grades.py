e5131176bd7b6f65d7eca7f3500a2f02039219d4
Cal Corbin <ccorbin@remmac06562.local>
2022-06-15 14:23:28 -0500

    First tutorials

---
+++ curving_test_grades.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+CURVE_CENTER = 80
+grades = np.array([72, 35, 64, 88, 51, 90, 74, 12])
+
+def curve(grades):
+    average = grades.mean()
+    change = CURVE_CENTER - average
+    new_grades = grades + change
+
+    # Increase scores, but don't allow them to go above 100.
+    return np.clip(new_grades, grades, 100)
+
+curvedGrades = curve(grades)
+print(curvedGrades)
